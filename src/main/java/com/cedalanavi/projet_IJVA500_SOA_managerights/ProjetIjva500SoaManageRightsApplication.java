4a048298b1b71e2a784dc8d34064a31a783e59d5
Neimaad <damienlaoussing@hotmail.com>
2022-11-14 15:48:58 +0100

    first commit

---
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_managerights/ProjetIjva500SoaManageRightsApplication.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_managerights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIjva500SoaManageRightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIjva500SoaManageRightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedalanavi/projet_IJVA500_SOA_managerights/ProjetIjva500SoaManageRightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_managerights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIjva500SoaManageRightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
